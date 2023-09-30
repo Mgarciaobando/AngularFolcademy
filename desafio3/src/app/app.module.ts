d7ef432dacd7d83b2c364f94cb3992a8c1f976e9
Mgarciaobando <103199160+Mgarciaobando@users.noreply.github.com>
2023-09-30 13:38:41 -0500

    Add files via upload

---
+++ desafio3/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutModule } from './components/layout/layout.module';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from './components/shared/shared.module';
+import { RoutesModule } from './components/routes/routes.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LayoutModule,
+    RouterModule,
+    SharedModule,
+    RoutesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
